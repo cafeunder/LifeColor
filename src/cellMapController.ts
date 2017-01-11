bdbfd7d30341f41e2f52bdebf353cbba4cfaf4a2
cafeunder <cafe3641@gmail.com>
2017-01-12 06:02:32 +0900

    描画用プロパティを定義

--- src/cellMapController.ts
+++ src/cellMapController.ts
@@ -1,21 +1,39 @@
 
+interface CellProperty {
+	cellSize: number;
+	gridWidth: number;
+}
+
 class CellMapController {
+	cellPropertyArray: CellProperty[];
 	alternationCount: number;
 	alternationInterval: number;
 	cellMap: CellMap;
 	cellMapView: CellMapView;
-	cellSize: number;
+	cellPropertyIndex: number;
 
 	constructor(domController: DOMController) {
+		this.cellPropertyArray = [{
+			cellSize: 16,
+			gridWidth: 4
+		}, {
+			cellSize: 8,
+			gridWidth: 2,
+		}, {
+			cellSize: 4,
+			gridWidth: 0
+		}]
+
 		this.alternationCount = 0;
 		this.alternationInterval = null;
-		this.cellSize = 16;
+		this.cellPropertyIndex = 0;
 
+		const cellProperty = this.cellPropertyArray[this.cellPropertyIndex];
 		this.cellMap = new CellMap(
-			Math.floor(domController.container.offsetWidth / this.cellSize),
-			Math.floor(domController.container.offsetHeight / this.cellSize),
+			Math.floor(domController.container.offsetWidth / cellProperty.cellSize),
+			Math.floor(domController.container.offsetHeight / cellProperty.cellSize),
 		);
-		this.cellMapView = new CellMapView(this.cellMap, this.cellSize, domController);
+		this.cellMapView = new CellMapView(this.cellMap, cellProperty, domController);
 	}
 
 	update(): void {
--- src/cellMapView.ts
+++ src/cellMapView.ts
@@ -7,11 +7,11 @@ class CellMapView {
 	gridCanvas: HTMLCanvasElement;
 	gridCanvasDrawer: CanvasDrawer;
 
-	cellSize: number;
+	cellProperty: CellProperty;
 	cellColor: Color;
 
-	constructor(cellMap: CellMap, cellSize: number, domController: DOMController) {
-		this.cellSize = cellSize;
+	constructor(cellMap: CellMap, cellProperty: CellProperty, domController: DOMController) {
+		this.cellProperty = cellProperty;
 		this.domController = domController;
 		this.cellMap = cellMap;
 
@@ -20,8 +20,9 @@ class CellMapView {
 		this.cellCanvasDrawer = new CanvasDrawer(this.cellCanvas);
 		this.gridCanvasDrawer = new CanvasDrawer(this.gridCanvas);
 
-		var width = this.cellSize * Math.floor(this.domController.container.offsetWidth / this.cellSize);
-		var height = this.cellSize * Math.floor(this.domController.container.offsetHeight / this.cellSize);
+		const cellSize = this.cellProperty.cellSize;
+		var width = cellSize * Math.floor(this.domController.container.offsetWidth / cellSize);
+		var height = cellSize * Math.floor(this.domController.container.offsetHeight / cellSize);
 		var left = Math.floor((this.domController.container.offsetWidth - width) / 2);
 		var top = Math.floor((this.domController.container.offsetHeight - height) / 2);
 		this.cellCanvasDrawer.changeCanvas(left, top, width, height);
@@ -35,27 +36,28 @@ class CellMapView {
 	}
 
 	draw(): void {
+		const cellSize = this.cellProperty.cellSize;
 		for (var y = 0; y < this.cellMap.yNum; ++y) {
 			for (var x = 0; x < this.cellMap.xNum; ++x) {
 				if (this.cellMap.map[y][x]) {
 					// 描画左上ピクセルのアルファ値がゼロ = 未描画なら描画
-					if (this.cellCanvasDrawer.getPixel(x * this.cellSize, y * this.cellSize, 3) == 0) {
+					if (this.cellCanvasDrawer.getPixel(x * cellSize, y * cellSize, 3) == 0) {
 						this.cellCanvasDrawer.drawRect(
-							x * this.cellSize,
-							y * this.cellSize,
-							this.cellSize,
-							this.cellSize,
+							x * cellSize,
+							y * cellSize,
+							cellSize,
+							cellSize,
 							this.cellColor
 						);
 					}
 				} else {
 					// 描画左上ピクセルのアルファ値が非ゼロ = 描画済ならクリア
-					if (this.cellCanvasDrawer.getPixel(x * this.cellSize, y * this.cellSize, 3) != 0) {
+					if (this.cellCanvasDrawer.getPixel(x * cellSize, y * cellSize, 3) != 0) {
 						this.cellCanvasDrawer.drawRect(
-							x * this.cellSize,
-							y * this.cellSize,
-							this.cellSize,
-							this.cellSize,
+							x * cellSize,
+							y * cellSize,
+							cellSize,
+							cellSize,
 							{
 								r: 0,
 								g: 0,
